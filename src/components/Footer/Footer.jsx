7a347f2811c0bea9d9aec0138ed523659705c827
Yevheniia Uhnich <kjaine@gmail.com>
2025-08-08 09:15:40 +0300

    currentYear

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import s from './Footer.module.css';
 
 const Footer = () => {
+  const currenYear = new Date().getFullYear();
   return (
     <div>
       <hr className={s.line} />
-      <p className={s.text}>© 2025 Yevheniia Uhnich ✦ Inspired Originals</p>
+      <p className={s.text}>© {currenYear} Yevheniia Uhnich ✦ Inspired Originals</p>
     </div>
   )
 }
